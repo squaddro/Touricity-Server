57c9cb043a1f718f358e9eb65140e26ce4e38153
BatuhanUlucay <hulucay@etu.edu.tr>
2020-02-14 00:13:16 +0300

    gerekli constructorlar eklendi.

--- src/main/java/com/squadro/touricity/message/types/data/Stop.java
+++ src/main/java/com/squadro/touricity/message/types/data/Stop.java
@@ -18,6 +18,15 @@ public class Stop extends Entry implements IStop {
 		this.duration = duration;
 		this.comment = comment;
 	}
+
+	public Stop(String location_id, String stop_id, int expense, int duration, String comment, int index){
+		this.location_id = location_id;
+		this.stop_id = stop_id;
+		this.expense = expense;
+		this.duration = duration;
+		this.comment = comment;
+		this.index = index;
+	}
 	
 	public String getLocation_id() {
 		return location_id;
