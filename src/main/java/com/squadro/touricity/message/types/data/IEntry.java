5c6fff740aee69fe8043f7bab5b3b625886f4f3f
BatuhanUlucay <hulucay@etu.edu.tr>
2020-02-19 12:59:43 +0300

    bugfix

--- src/main/java/com/squadro/touricity/message/types/data/IEntry.java
+++ src/main/java/com/squadro/touricity/message/types/data/IEntry.java
@@ -1,5 +1,22 @@
 package com.squadro.touricity.message.types.data;
 
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+
+@JsonTypeInfo(use = JsonTypeInfo.Id.NAME,
+include = JsonTypeInfo.As.PROPERTY,
+property = "type")
+@JsonSubTypes({
+		@JsonSubTypes.Type(
+				value = Stop.class,
+				name = "stop"
+		),
+		@JsonSubTypes.Type(
+				value = Path.class,
+				name = "path"
+		)
+}
+)
 public interface IEntry extends IDataType {
 	int getDuration();
 	int getExpense();
