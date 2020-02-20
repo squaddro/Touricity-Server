b72642c114ad7efb7b6a937abe3f4fd8cb298b5b
Yasar Can <ykakdas@gmail.com>
2020-02-21 01:05:13 +0300

    thread safe implementation tried

--- src/main/java/com/squadro/touricity/database/query/routeQueries/CreatorSelectionFromRouteId.java
+++ src/main/java/com/squadro/touricity/database/query/routeQueries/CreatorSelectionFromRouteId.java
@@ -4,15 +4,18 @@ import com.squadro.touricity.database.query.SelectionQuery;
 import com.squadro.touricity.database.result.QueryResult;
 
 import java.sql.SQLException;
+import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.atomic.AtomicReference;
 
 public class CreatorSelectionFromRouteId extends SelectionQuery {
 
     private final AtomicReference<String> creator = new AtomicReference<>();
     private final String route_id;
+    private final CountDownLatch countDownLatch;
 
     public CreatorSelectionFromRouteId(String route_id) {
         this.route_id = route_id;
+        countDownLatch = new CountDownLatch(1);
     }
 
     @Override
@@ -24,12 +27,17 @@ public class CreatorSelectionFromRouteId extends SelectionQuery {
     public boolean onResult(QueryResult result) throws SQLException {
         if(result.getResultSet().next()){
             creator.set(result.getResultSet().getString("creator"));
+            countDownLatch.countDown();
         }
         return false;
     }
 
     public String getCreator(){
+        try {
+            countDownLatch.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         return creator.get();
     }
-
 }
