8d32bdb88839e04a59d8411cb4930f8394fb263c
furkandinc <furkandinc@etu.edu.tr>
2020-03-05 19:37:44 +0300

    PathVertex database tipi değişti

---
+++ src/main/java/com/squadro/touricity/database/ByteArrays.java
@@ -0,0 +1,62 @@
+package com.squadro.touricity.database;
+
+import com.squadro.touricity.database.query.routeQueries.InsertNewEntryQuery;
+import com.squadro.touricity.message.types.data.IPath;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.nio.ByteBuffer;
+import java.nio.DoubleBuffer;
+import java.util.Base64;
+
+public class ByteArrays {
+	protected static Logger  logger = LoggerFactory.getLogger(ByteArrays.class);
+	public static class Encoders {
+		public static String encodePathVertexArray(IPath.PathVertex[] vertexArray) {
+			double[] doubleArray = new double[vertexArray.length * 2];
+			int i=0;
+			for (IPath.PathVertex pathVertex : vertexArray) {
+				doubleArray[i++] = pathVertex.getLatitude();
+				doubleArray[i++] = pathVertex.getLongitude();
+			}
+
+			return encodeDoubleArray(doubleArray);
+		}
+
+		public static String encodeDoubleArray(double[] doubleArray) {
+			String str = Base64.getEncoder().encodeToString(Converters.doubleArrayToByteArray(doubleArray));
+			return str;
+		}
+	}
+
+	public static class Decoders {
+		public static double[] decodeDoubleArrays(String base64Encoded) {
+			return Converters.byteToDoubleArray(Base64.getDecoder().decode(base64Encoded));
+		}
+
+		public static IPath.PathVertex[] decodePathVertexArray(String base64Encoded) {
+			double[] doubles = decodeDoubleArrays(base64Encoded);
+			IPath.PathVertex[] pathVertices = new IPath.PathVertex[doubles.length/2];
+
+			for(int i=0; i<doubles.length; i+=2)
+				pathVertices[i] = new IPath.PathVertex(doubles[i], doubles[i+1]);
+
+			return pathVertices;
+		}
+	}
+
+	public static class Converters {
+		public static byte[] doubleArrayToByteArray (double[] array) {
+			ByteBuffer buf = ByteBuffer.allocate(Double.SIZE / Byte.SIZE * array.length);
+			buf.asDoubleBuffer().put(array);
+			return buf.array();
+		}
+
+		public static double[] byteToDoubleArray(byte[] bytes) {
+			DoubleBuffer buf = ByteBuffer.wrap(bytes).asDoubleBuffer();
+			double[] doubleArray = new double[buf.limit()];
+			buf.get(doubleArray);
+			return doubleArray;
+		}
+	}
+}
--- src/main/java/com/squadro/touricity/database/query/routeQueries/PathListSelectionFromRouteId.java
+++ src/main/java/com/squadro/touricity/database/query/routeQueries/PathListSelectionFromRouteId.java
@@ -1,9 +1,13 @@
 package com.squadro.touricity.database.query.routeQueries;
 
+import com.squadro.touricity.database.ByteArrays;
+import com.squadro.touricity.database.Database;
 import com.squadro.touricity.database.query.SelectionQuery;
 import com.squadro.touricity.database.result.QueryResult;
+import com.squadro.touricity.message.types.data.IPath;
 import com.squadro.touricity.message.types.data.Path;
 
+import javax.xml.crypto.Data;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
@@ -32,11 +36,12 @@ public class PathListSelectionFromRouteId extends SelectionQuery {
 		if (result.isSuccessfull()) {
 			do {
 				path = new Path();
-				path.setPath_id(rs.getString("path_id"));
-				path.setExpense(rs.getInt("expense"));
-				path.setDuration(rs.getInt("duration"));
-				path.setComment(rs.getString("comment_desc"));
-				path.setIndex(rs.getInt("pointer"));
+				path.setPath_id(rs.getString(Database.PATH_PATH_ID));
+				path.setExpense(rs.getInt(Database.ENTRY_EXPENSE));
+				path.setDuration(rs.getInt(Database.ENTRY_DURATION));
+				path.setComment(rs.getString(Database.ENTRY_COMMENT_DESCRIPTION));
+				path.setIndex(rs.getInt(Database.ENTRY_POINTER));
+				path.setVertices(ByteArrays.Decoders.decodePathVertexArray(rs.getString(Database.PATH_VERTICES)));
 
 				list.add(path);
 			} while (rs.next());
