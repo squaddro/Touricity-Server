37dd8500a0bece2f78ecd685096191a4e9d478a7
BatuhanUlucay <hulucay@etu.edu.tr>
2020-04-16 14:12:03 +0300

    bugfix

--- src/main/java/com/squadro/touricity/controller/SuggestedRoutesController.java
+++ src/main/java/com/squadro/touricity/controller/SuggestedRoutesController.java
@@ -6,6 +6,7 @@ import com.squadro.touricity.message.types.data.RouteSuggestion;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
+@RestController
 public class SuggestedRoutesController {
     @RequestMapping(
             value = "/suggestedRoutes",
